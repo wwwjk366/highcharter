a2dc49c04b9db217a292e6c4a88e0731db95ba45
Joshua <jbkunst@gmail.com>
2017-01-12 12:58:25 -0300

    addig more charts logo etc

--- _scripts/helpers.R
+++ _scripts/helpers.R
@@ -1,44 +1,41 @@
 get_demos <- function(){
-  library("highcharter")
-  library("forecast")
-  options(highcharter.theme = hc_theme_smpl())
-  data("citytemp")
+  library(highcharter)
+  library(forecast)
+  library(purrr)
+  library(viridisLite)
+  library(igraph)
+  library(dplyr)
+  library(mvtnorm)
+  library(tidyr)
+  
+  options(highcharter.theme = hc_theme_smpl(
+    plotOptions = list(
+      series = list(showInLegend = FALSE),
+      line = list(lineWidth = 1)),
+    tooltip = list(valueDecimals = 2)
+    )
+  )
+  
   data(diamonds, mpg, package = "ggplot2")
-  
-  p1 <- hchart(mpg, "scatter", hcaes(x = displ, y = hwy, color = hwy))
-  
-  p2 <- hchart(
-    forecast(auto.arima(AirPassengers), level = 95, h = 12*3),
-    showInLegend = FALSE) %>% 
-    hc_tooltip(valueDecimals = 2) %>% 
-    hc_add_theme(hc_theme_538())
-  
-  p3 <- highcharts_demo()%>% 
-    hc_add_theme(hc_theme_economist()) %>% 
-    hc_plotOptions(series = list(showInLegend = FALSE)) %>% 
-    hc_title(text = "") %>% hc_subtitle(text = "")
-  
-  data(worldgeojson)
   data(GNI2014, package = "treemap")
-  head(GNI2014)
-  library("viridisLite")
   
-  dshmstops <- data.frame(
-    q = c(0, exp(1:5)/exp(5)),
-    c = substring(viridis(5 + 1), 0, 7)) %>% 
-    list_parse2()
   
-  p4 <- hcmap(data = GNI2014, name = "", value = "GNI", joinBy = c("iso-a3", "iso3")) %>% 
-    hc_colorAxis(stops = dshmstops) %>% 
+  p1 <- hchart(mpg, "scatter", hcaes(x = displ, y = hwy, color = hwy), name = "Cars")
+  
+  p2 <- hchart(forecast(auto.arima(AirPassengers), level = 95, h = 12*3))
+  
+  p3 <- hcmap(data = GNI2014, name = "", value = "GNI",
+              joinBy = c("iso-a3", "iso3"), nullColor = "#FAFAFA") %>% 
+    hc_colorAxis(stops = color_stops(colors = inferno(10, begin = 0.1)),
+                 type = "logarithmic") %>% 
     hc_legend(enabled = FALSE) %>% 
-    hc_add_theme(hc_theme_db()) %>% 
     hc_mapNavigation(enabled = FALSE)
   
-  p5 <- hcboxplot(diamonds$price, var = diamonds$color, outliers = FALSE, name = "price") %>% 
+  p4 <- hcboxplot(diamonds$price, var = diamonds$color,
+                  outliers = FALSE, name = "price") %>% 
     hc_yAxis(min = 0)
   
   
-  library("igraph")
   N <- 20
   net <- sample_gnp(N, p = .1)
   wc <- cluster_walktrap(net)
@@ -49,39 +46,54 @@ get_demos <- function(){
   V(net)$degree <- degree(net)
   V(net)$size <- V(net)$degree
   V(net)$comm <- membership(wc)
-  V(net)$color <- colorize(membership(wc), viridis::inferno(10))
+  V(net)$color <- colorize(membership(wc), magma(length(wc)))
+  p5 <- hchart(net, layout = layout_with_fr)
   
-  p6 <- hchart(net, layout = layout_with_fr)
+  p6 <- hcwaffle(c("car", "truck", "plane"), c(10, 7, 5),
+                 icons = c("car", "truck", "plane")) %>% 
+    hc_colors(hex_to_rgba(viridis(3, end = .8)))
+  
+  p7 <- highchart() %>% 
+    hc_add_series(density(rnorm(100000)),
+                  type = "area", name = "Normal Distribution") %>% 
+    hc_add_series(density(rgamma(100000, 5, 0.8)),
+                  type = "area", name = "Gamma(5. 0.8) Distribution") %>%
+    hc_add_series(density(rgamma(100000, 3, 0.8)),
+                  type = "area", name = "Gamma(3. 0.8) Distribution") %>% 
+    hc_plotOptions(series = list(fillOpacity = 0.5)) %>% 
+    hc_xAxis(min = -5, max = 12) 
   
   
-  H <- 250
-  p1$height <- H
-  p2$height <- H
-  p3$height <- H
-  p4$height <- H
-  p5$height <- H
-  p6$height <- H
+  brks <- seq(-3, 3, length.out = 40)
+  grid <- expand.grid(brks, brks)
+  m <- as.data.frame(grid) %>% 
+    mutate(value =
+             dmvnorm(grid, mean = c(1, 1), sigma = matrix(c(1, .7, .7, 1), nrow = 2)) +
+             dmvnorm(grid, mean = c(-1, -1), sigma = matrix(c(1, -.7, -.7, 1), nrow = 2)) +
+             dmvnorm(grid, mean = c(2, -2), sigma = matrix(c(1, 0, 0, 1), nrow = 2))) %>% 
+    spread(Var2, value) %>% 
+    select(-Var1) %>% 
+    as.matrix() 
   
-  demos <- htmltools::tagList(
-    tags$div(
-      class = "row",
-      tags$div(class = "col-sm-6", p1),
-      tags$div(class = "col-sm-6", p2)
-    ),
-    tags$div(
-      class = "row",
-      tags$div(class = "col-sm-6", p3),
-      tags$div(class = "col-sm-6", p4)
-    ),
-    tags$div(
-      class = "row",
-      tags$div(class = "col-sm-6", p5),
-      tags$div(class = "col-sm-6", p6)
-    )
+  p8 <- hchart(m) %>% 
+    hc_add_theme(hc_theme_null()) %>% 
+    hc_legend(enabled = FALSE) %>% 
+    hc_colorAxis(stops = color_stops(colors = inferno(10, begin = 0.1)))
+  
+  
+  plots <- list(p1, p2, p3, p4, p5, p6, p7, p8)
+  
+  plots <- map(plots, hc_size, height = 250)
+  
+  divplots <- map(plots, tags$div, class = "col-md-6")
+  
+  divplots <- htmltools::tagList(
+    tags$div(class = "row", divplots)
   )
   
-  # htmltools::browsable(demos)
-  demos  
+  # htmltools::browsable(divplots)
+  
+  divplots  
   
   
 }
--- _scripts/index.R
+++ _scripts/index.R
@@ -2,9 +2,10 @@
 #' layout: post
 #' ---
 #' 
-#' ## Welcome to the Highcharter homepage
-#'
 
+#' 
+#' # <span id="brand">h|1i|0g|3h|2c|1h|2a|1r|3t|2e|1r|2{rpackage}</span>
+#' 
 #+echo=FALSE
 rm(list = ls())
 try(source("_scripts/helpers.R"))
