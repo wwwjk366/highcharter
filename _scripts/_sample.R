10a135f1659862f2c582dfa50e30b9a0eded8e96
Joshua <jbkunst@gmail.com>
2017-01-18 01:13:57 -0300

    adding samples

--- _scripts/_sample.R
+++ _scripts/_sample.R
@@ -1,34 +1,50 @@
 library(dplyr)
+library(highcharter)
+library(stringr)
+library(purrr)
 
+options(highcharter.theme = hc_theme_smpl())
 
-n <- 5
+n <- 4
 
-set.seed(123)
+set.seed(124)
 
-df <- data_frame(
-  id = seq_len(n),
-  name = sample(fruit, size = n),
-  x = id - 1,
-  y = 1 + x + sample(x/n*5),
-  value = y
-)
+df <- data.frame(x = seq_len(n) - 1) %>% 
+  mutate(
+    y = 10 + x + 10 * sin(x),
+    y = round(y, 1),
+    z = (x*y) - median(x*y),
+    e = 10 * abs(rnorm(length(x))) + 2,
+    e = round(e, 1),
+    low = y - e,
+    high = y + e,
+    value = x,
+    name = sample(fruit, size = n),
+    color = head(getOption("highcharter.theme")$color, n)
+  )
 
-hc <- highchart() %>%
-  hc_xAxis(categories = df$name) %>% 
-  hc_add_series(df, name = "Fruit Consumption", showInLegend = FALSE) %>%
-  hc_add_theme(hc_theme_smpl())
+df
 
-cbp <- c("line", "spline", "area")
+create_hc <- function(t) {
+  
+  dont_rm_high_and_low <- c("arearange", "areasplinerange",
+                            "columnrange", "errorbar")
+  
+  if(! t %in% dont_rm_high_and_low) df <- df %>% select(-e, -low, -high)
+  
+  highchart() %>%
+    hc_title(text = t) %>% 
+    hc_chart(type = t) %>% 
+    hc_xAxis(categories = df$name) %>% 
+    hc_add_series(df, name = "Fruit Consumption", showInLegend = FALSE) 
 
-c("line", "column", "waterfall" , "funnel", "pie" , "treemap", "scatter",
-  "spline", "area")  %>% 
-  map(function(t) {
-    hc %>%
-      hc_title(text = t) %>% 
-      hc_chart(type = t) %>% 
-      hc_plotOptions(series = list(colorByPoint = ! t %in% cbp))
-  }) %>% 
+}
+
+c("line", "spline",  "area", "areaspline",
+  "column", "bar", "waterfall" , "funnel", "pyramid",
+  "pie" , "treemap", "scatter", "bubble",
+  "arearange", "areasplinerange", "columnrange", "errorbar",
+  "polygon")  %>% 
+  map(create_hc) %>% 
   hw_grid() %>% 
   htmltools::browsable()
-
-
